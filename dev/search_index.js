eb88e0c28388e4e14dcabf6bf7876f3f852d41b4
Documenter.jl <documenter@juliadocs.github.io>
2024-01-22 11:34:17 +0000

    build based on 853d34d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EMPJuMP","category":"page"},{"location":"#EMPJuMP","page":"Home","title":"EMPJuMP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EMPJuMP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EMPJuMP]","category":"page"}]
+}
